0f1faa2807604814d0e6edb21a799d163372c58f
JaeyoonCheon <cjswodbs50@gmail.com>
2022-02-08 12:24:56 +0900

    6064 / right answer

--- BOJ/Bruteforce/6064.py
+++ BOJ/Bruteforce/6064.py
@@ -1,5 +1,14 @@
 """
 6064 카잉 달력
+
+1. x, y를 하나씩 증가시키는 방법
+이 방법은 정답을 찾아낼 수 있으나 시간복잡도가 O(M*N)으로 간주
+시간 제한을 넘길 수 없었음
+
+2. x, y 둘 중 하나를 고정시키고 다른 하나만 증가시켜 나누어 반복
+우선, x와 y가 같아지는 지점까지 증가시킨 후, 더 작은 쪽의 최대 수로 나누어 시계 처럼
+증가 후 나머지 연산을 하여 중간의 필요없는 연산을 축약
+나누어 나머지가 0이 되는 지점의 예외처리가 이 코드에서는 필요(예외 없이 작성 가능할 듯)
 """
 
 def GCD(a, b):
@@ -40,30 +49,39 @@ def calculateFast(M, N, x, y):
 
     xStart = 1
     yStart = 1
-    
-    while xStart != x and yStart != y:
-        count += 1
-        xStart += 1
-        yStart += 1
 
     if M > N:
         divisor = "right"
+        while yStart != y:
+            count += 1
+            xStart += 1
+            yStart += 1
     else:
         divisor = "left"
-
-    while count < maxCount:
+        while xStart != x:
+            count += 1
+            xStart += 1
+            yStart += 1
+    
+    while count <= maxCount:
         if xStart == x and yStart == y:
             rightAnswer = True
             break
         
         if divisor == "right":
-            yStart += M
-            yStart %= N
-            count += M
+            xStart += N
+            if xStart % M == 0:
+                count+=N
+            else:
+                xStart %= M
+                count += N
         else:
-            yStart += N
-            yStart %= M
-            count += N
+            yStart += M
+            if yStart % N == 0:
+                count+=M
+            else:
+                yStart %= N
+                count += M
             
         
     return count if rightAnswer else -1
