ec6b0dad5b95977e7c2857d208ee880388d7db18
JaeyoonCheon <cjswodbs50@gmail.com>
2022-04-05 11:41:58 +0900

    10866 / right answer

---
+++ BOJ/Graph/10866.py
@@ -0,0 +1,44 @@
+"""
+10866번 덱
+
+1.  덱의 간단한 구현
+"""
+
+N = int(input())
+
+q = []
+
+operation = [list(input().split()) for _ in range(N)]
+
+for i in operation:
+    if i[0] == "push_front":
+        q.append(int(i[1]))
+    elif i[0] == "pop_front":
+        if len(q) == 0:
+            print("-1")
+        else:
+            print(q.pop(len(q) - 1))
+    elif i[0] == "push_back":
+        q.insert(0, int(i[1]))
+    elif i[0] == "pop_back":
+        if len(q) == 0:
+            print("-1")
+        else:
+            print(q.pop(0))
+    elif i[0] == "size":
+        print(len(q))
+    elif i[0] == "empty":
+        if len(q) == 0:
+            print("1")
+        else:
+            print("0")
+    elif i[0] == "back":
+        if len(q) == 0:
+            print("-1")
+        else:
+            print(q[0])
+    else:
+        if len(q) == 0:
+            print("-1")
+        else:
+            print(q[len(q) - 1])
