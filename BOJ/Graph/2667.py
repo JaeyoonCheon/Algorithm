41e04808062ed9c45f3717fb4c857502e39d7544
JaeyoonCheon <cjswodbs50@gmail.com>
2022-04-07 00:06:15 +0900

    2667 / right answer

--- BOJ/Graph/2667.py
+++ BOJ/Graph/2667.py
@@ -1,5 +1,11 @@
 """
 2667번 단지 번호 붙이기
+
+1.  dfs는 일반적으로 동작하는 방식대로 연결된 단지 중 방문하지 않은 집을 방문할 때 마다
+    갯수를 증가시켜 따로 저장해놓는다.
+    
+2.  N*N 격자를 모두 검사하면서 단지에 해당하지만 방문하지 않은 단지를 dfs 실행하여 단지의
+    수를 검사한다.
 """
 
 N = int(input())
@@ -16,6 +22,8 @@ def dfs(x, y, grid, visited):
 
     for i in range(-1, 2):
         for j in range(-1, 2):
+            if i == j or i == -1 * j:
+                continue
             if x + i >= N or x + i < 0:
                 continue
             if y + j >= N or y + j < 0:
@@ -38,7 +46,7 @@ for i in range(N):
             number += 1
             dfs(i, j, grid, visited)
             numbers.append(number)
-            number = 0
+            number = -1
 
 numbers.sort()
 
