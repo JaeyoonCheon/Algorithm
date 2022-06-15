0fa59fc1776b41ef73837d6779f0c7e0f0cad256
JaeyoonCheon <cjswodbs50@gmail.com>
2022-06-15 23:56:46 +0900

    2448

--- BOJ/2448.py
+++ BOJ/2448.py
@@ -2,6 +2,10 @@
 2448번 별 찍기 11
 """
 
+import sys
+
+sys.setrecursionlimit(1000000)
+
 N = int(input())
 
 paper = [[" "] * (2 * N - 1) for _ in range(N)]
@@ -9,24 +13,38 @@ paper = [[" "] * (2 * N - 1) for _ in range(N)]
 step = 0
 for i in range(N):
     for j in range(N - 1 - step, N + step):
-        paper[i][j] = "*"
+        paper[i][j] = " "
     step += 1
 
-print()
+
+def makeStar(top, bottom, left, right):
+    halfV = (bottom - top) // 2
+    halfH = (right - left) // 2
+
+    paper[top][(right + left) // 2] = "*"
+    paper[top + halfV][left + 1] = "*"
+    paper[top + halfV][right - 1] = "*"
+    paper[bottom][left] = "*"
+    paper[bottom][left + 1] = "*"
+    paper[bottom][(right + left) // 2] = "*"
+    paper[bottom][right - 1] = "*"
+    paper[bottom][right] = "*"
 
 
-def star(top, left, right):
-    if right - left <= 1:
-        paper[top][left] = "*"
+def star(top, bottom, left, right):
+    halfV = (bottom - top) // 2
+    halfH = (right - left) // 2
+
+    if halfV == 2:
+        makeStar(top, bottom, left, right)
         return
 
-    half = (right - left) // 2
-    star(top, left + half // 2, right - half // 2)
-    star(top // 2, left, half)
-    star(top // 2, half, right)
+    star(top, top + halfV, left + halfH // 2, right - halfH // 2)
+    star(top + halfV, bottom, left, halfH)
+    star(top + halfV, bottom, halfH, right)
 
 
-star(N, 0, 2 * N - 1)
+star(0, N - 1, 0, 2 * N - 1 - 1)
 
 for line in paper:
     print(line)
