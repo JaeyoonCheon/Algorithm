dc5bc8274878a9bec11050563969233e06fff1fa
JaeyoonCheon <cjswodbs50@gmail.com>
2022-07-15 14:56:30 +0900

    2252

--- BOJ/2252.py
+++ BOJ/2252.py
@@ -2,19 +2,46 @@
 2252번 줄 세우기
 """
 
-import sys
+import sys, collections
 
 N, M = map(int, input().split())
 
 G = [[] for _ in range(N + 1)]
+invertedG = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     _from, _to = map(int, sys.stdin.readline().split())
 
     G[_from].append(_to)
+    invertedG[_to].append(_from)
 
 
 income = [()]
 
 for i in range(1, N + 1):
-    income.append((i, len(G[i])))
+    income.append(len(invertedG[i]))
+
+path = []
+
+q = collections.deque()
+
+step = 0
+
+for i in range(1, N + 1):
+    if income[i] == 0 and i not in path and i not in q:
+        q.append((i, 0))
+        path.append(i)
+
+while q:
+    curr = q.popleft()
+
+    for nextNode in G[curr]:
+        income[nextNode] -= 1
+
+    for i in range(1, N + 1):
+        if income[i] == 0 and i not in path and i not in q:
+            q.append((i, curr[1] + 1))
+            path.append(i)
+
+for i in path:
+    print(i, end=" ")
