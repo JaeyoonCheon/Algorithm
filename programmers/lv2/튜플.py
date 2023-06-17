d0a89611d69bfba42177d21b4d758d772b2613ec
JaeyoonCheon <cjswodbs50@gmail.com>
2023-06-18 00:07:47 +0900

    튜플 / 딕셔너리 풀이 추가

--- programmers/lv2/튜플.py
+++ programmers/lv2/튜플.py
@@ -3,11 +3,15 @@
     
     1.  문자열로 주어지는 입력값을 리스트로 파싱
     2.  파싱된 리스트를 길이 순 오름차순 정렬한 뒤, 작은 것 부터 새로 추가되는 수를 순서대로 추가
+
+    3.  추가 - in 연산자를 사용해 중복 체크를 할 경우, 해시를 이용한 딕셔너리를 적용하여 결과값으로 반환 가능.
+        딕셔너리를 리스트로 형 변환 시 초기화한 순서대로 리스트에 적용됨.
 """
 
 
 def solution(s):
     answer = []
+    answerDict = {}
 
     arrays = s[2:-2].split("},{")
 
@@ -16,10 +20,10 @@ def solution(s):
 
     for array in arrays:
         for i in array:
-            if i not in answer:
-                answer.append(i)
+            if i not in answerDict:
+                answerDict[i] = True
 
-    return answer
+    return list(answerDict)
 
 
 solution("{{4,2,3},{3},{2,3,4,1},{2,3}}")
